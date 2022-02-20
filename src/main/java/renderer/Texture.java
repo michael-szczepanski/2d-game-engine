7a8f5f212f0dabb100a4785cae62d728dbf8c074
BetterAshThanDust <h4y473@gmail.com>
2022-02-20 15:52:43 +0000

    spritesheets functionality implemented

--- src/main/java/renderer/Texture.java
+++ src/main/java/renderer/Texture.java
@@ -14,6 +14,7 @@ import static org.lwjgl.stb.STBImage.stbi_set_flip_vertically_on_load;
 public class Texture {
     private String filepath;
     private int texID;
+    private int width, height;
 
     public Texture(String filepath) {
         this.filepath = filepath;
@@ -38,6 +39,9 @@ public class Texture {
         ByteBuffer image = STBImage.stbi_load(filepath, width, height, channels, 0);
 
         if (image != null) {
+            this.width = width.get(0);
+            this.height = height.get(0);
+
             if (channels.get(0) == 4) {
                 glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width.get(0), height.get(0), 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
             } else if (channels.get(0) == 3) {
@@ -58,5 +62,13 @@ public class Texture {
 
     public void unbind() {
         glBindTexture(GL_TEXTURE_2D, 0);
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
     }
 }
